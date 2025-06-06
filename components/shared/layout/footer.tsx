10ace462cfc08ae8f7a2b1045c7626b0af2fc073
MV <maxv@uci.edu>
2025-06-06 03:55:44 -0700

    using metaplanet.jp footer

--- components/shared/layout/footer.tsx
+++ components/shared/layout/footer.tsx
@@ -1,9 +1,5 @@
-import Link from 'next/link';
 
-import LogoSquare from '@/components/shared/logo-square';
-import FooterMenu from 'components/shared/layout/footer-menu';
 import { getMenu } from 'lib/shopify';
-import { Suspense } from 'react';
 
 // i18n
 import { getLocale, getTranslations } from 'next-intl/server';
@@ -20,53 +16,45 @@ export default async function Footer() {
   const skeleton = 'w-full h-6 animate-pulse rounded-sm bg-neutral-200 dark:bg-neutral-700';
   const menu = await getMenu('next-js-frontend-footer-menu');
   const copyrightName = COMPANY_NAME || SITE_NAME || '';
-
   return (
-    <footer className="text-sm text-neutral-500 dark:text-neutral-400">
-      <div className="mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-200 px-6 py-12 text-sm md:flex-row md:gap-12 md:px-4 min-[1320px]:px-0 dark:border-neutral-700">
+    <footer className="`w-full -pb-4 mt-auto bg-background">
+
+      <div
+        className={`container mx-auto text-center text-sm flex flex-col md:flex-row gap-4 items-end justify-center ${ "text-foreground/50"} /* ""}*/`}
+      >
         <div>
-          <Link className="flex items-center gap-2 text-black md:pt-1 dark:text-white" href="/">
-            <LogoSquare size="sm" />
-            <span className="uppercase">{SITE_NAME}</span>
-          </Link>
+          <div className="block md:inline">
+            ©️ {currentYear} Metaplanet Inc.
+          </div>
+          <div className="block md:inline">All Rights Reserved.</div>
+          <div className="block md:inline">
+            「Registered Trademark―登録商標」
+          </div>
         </div>
-        <Suspense
-          fallback={
-            <div className="flex h-[188px] w-[200px] flex-col gap-2">
-              <div className={skeleton} />
-              <div className={skeleton} />
-              <div className={skeleton} />
-              <div className={skeleton} />
-              <div className={skeleton} />
-              <div className={skeleton} />
-            </div>
-          }
-        >
-          <FooterMenu menu={menu} />
-        </Suspense>
-        <div className="md:ml-auto">
-            <span className="px-3">₿</span>
-            <hr className="h-full border-r border-neutral-200 dark:border-neutral-700" />
-        </div>
-      </div>
-      <div className="border-t border-neutral-200 py-6 text-sm dark:border-neutral-700">
-        <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 px-4 md:flex-row md:gap-0 md:px-4 min-[1320px]:px-0">
-          <p>
-            &copy; {copyrightDate} {copyrightName}
-            {copyrightName.length && !copyrightName.endsWith('.') ? '.' : ''} All rights reserved. 
-            {"「"+t('registeredTrademark')+"」"}
-          </p>
-          <hr className="mx-4 hidden h-4 w-[1px] border-l border-neutral-400 md:inline-block" />
-          <p>
-            <a href={"https://metaplanet.jp/"+locale}>{SITE_NAME}</a>
-          </p>
-          <p className="md:ml-auto">
-            <a href={"https://metaplanet.jp/"+locale} className="text-black dark:text-white">
-              {t('poweredBy')}
+        <div className="flex gap-3">
+          <div>
+            <a href="/about/contact-us" className="hover:text-foreground/70">
+              Contact Us
             </a>
-          </p>
+          </div>
+          <div>
+            <a href={`/privacy-policy`} className="hover:text-foreground/70">
+              Privacy Policy
+            </a>
+          </div>
+          <div>
+            <a
+              href="https://aqsmixvnbavrufgttwsn.supabase.co/storage/v1/object/public/media-kit/Media_Kit_Metaplanet.zip"
+              download
+              className="hover:text-foreground/70"
+            >
+              Media Kit
+            </a>
+          </div>
         </div>
       </div>
     </footer>
   );
 }
+
+
